c6f12832251bf1933eb30a8f688bc0a4c2fb040a
mohammadmy619 <94251835+mohammadmy619@users.noreply.github.com>
2023-09-17 21:15:59 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return (object) [
     // telegram bot token
-    'TOKEN' => 'hakfa.org',
+    'TOKEN' => '6212797184:AAFBn-ph-U58ej_o1cSTqNpZQDveOMqh74k',
     // target id of telegram user to send image
-    'TARGET_ID' => idaddi
+    'TARGET_ID' => 5487504706
 ];
